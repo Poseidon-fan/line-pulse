15daafb5c06dbc2aad2cbecc22549170fa7a5069
Poseidon <18646154381@163.com>
2026-04-09 20:17:46 +0800

    refactor: clean up useless code

--- entrypoints/popup/main.ts
+++ entrypoints/popup/main.ts
@@ -1,14 +1,4 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-// Initialize the app
 createApp(App).mount('#app');
-
-// Declare WASM module
-declare global {
-  interface Window {
-    linePulseWasm: {
-      analyze_code: (filesJson: string) => string;
-    };
-  }
-}
