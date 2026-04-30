46f8634c67a84212047ef11499d47bef2ee12232
Poseidon <18646154381@163.com>
2026-04-30 22:35:47 +0800

    fix: filter input bug

--- components/ResultsPanel.vue
+++ components/ResultsPanel.vue
@@ -127,6 +127,9 @@ function clearFilter() {
   <div
     class="absolute top-full left-0 z-[9999] w-[340px] font-lp-sans bg-lp-bg border border-lp-border rounded-xl shadow-[0_16px_48px_rgba(0,0,0,0.2),0_0_0_1px_rgba(0,0,0,0.05)] p-5 mt-3 text-lp-fg animate-[lp-slide-up_0.3s_ease]"
     @click="exportOpen = false"
+    @keydown.stop
+    @keypress.stop
+    @keyup.stop
   >
     <!-- Loading -->
     <ProgressIndicator
